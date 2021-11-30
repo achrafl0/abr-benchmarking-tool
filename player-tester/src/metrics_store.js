32120b191a45c079ebadd042b6c382c359c61aa1
Paul Berberian <pea.berberian@gmail.com>
2021-11-30 18:16:25 +0100

    improve event listening

--- player-tester/src/metrics_store.js
+++ player-tester/src/metrics_store.js
@@ -1,6 +1,3 @@
-import { CDN_SERVER_URL } from "./consts";
-
-
 export default class MetricsStore {
   /** indicate a time origin for the x axis */
   #timeRef;
@@ -11,30 +8,38 @@ export default class MetricsStore {
   playbackRates;
 
   constructor() {
-    this.#timeRef = performance.now();
-    this.#callbacks = [];
-    this.audioBitrates = [];
-    this.bufferSizes = [];
-    this.videoBitrates = [];
-    this.playbackRates = [];
+    this.reset();
   }
 
   reset() {
     this.#timeRef = performance.now();
-    this.#callbacks = [];
+    this.#callbacks = {};
     this.audioBitrates = [];
     this.bufferSizes = [];
     this.videoBitrates = [];
     this.playbackRates = [];
   }
 
-  addUpdateListener(fn) {
-    this.#callbacks.push(fn);
+  addUpdateListener(eventType, fn) {
+    let cbsForEvent = this.#callbacks[eventType];
+    if (cbsForEvent === undefined) {
+      cbsForEvent = [];
+      this.#callbacks[eventType] = cbsForEvent;
+    }
+    cbsForEvent.push(fn);
+
     return () => {
-      let indexOf = this.#callbacks.indexOf(fn);
+      const cbs = this.#callbacks[eventType];
+      if (cbs === undefined) {
+        return;
+      }
+      let indexOf = cbs.indexOf(fn);
       while (indexOf !== -1) {
-        this.#callbacks.splice(indexOf, 1);
-        indexOf = this.#callbacks.indexOf(fn);
+        cbs.splice(indexOf, 1);
+        indexOf = cbs.indexOf(fn);
+      }
+      if (cbs.length === 0) {
+        delete this.#callbacks[eventType];
       }
     };
   }
@@ -61,17 +66,7 @@ export default class MetricsStore {
       default:
         throw new Error("Unknown metric: " + eventType);
     }
-
-    this.#callbacks.forEach(cb => {
-      try {
-        cb();
-      } catch (e) {
-        const message = e && e.message ?
-          "No error message" :
-          e.message;
-        throw new Error("Event listener has thrown: " + message);
-      }
-    });
+    this.#triggerEventUpdate(eventType, eventData);
   }
 
   exportData() {
@@ -81,5 +76,22 @@ export default class MetricsStore {
       playbackRates: this.playbackRates,
       videoBitrates: this.videoBitrates,
     };
+  }
+
+  #triggerEventUpdate(eventType, eventData) {
+    const cbs = this.#callbacks[eventType];
+    if (cbs === undefined) {
+      return;
+    }
+    cbs.forEach(cb => {
+      try {
+        cb(eventData);
+      } catch (e) {
+        const message = e && e.message ?
+          "No error message" :
+          e.message;
+        throw new Error("Event listener has thrown: " + message);
+      }
+    });
   }
 }
